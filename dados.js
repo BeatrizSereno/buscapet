f322ae7f83ae462cb4f7789a0935369282cce17a
BeatrizSereno <beatrizzsereno@gmail.com>
2024-09-07 23:53:39 -0300

    Add files via upload
    O Busca Pet é um site para tutores de primeira viagem buscarem por respostas sobre suas dúvidas mais comuns ao adquirirem um pet. Lá eles irão encontrar informações sobre vacinas, alimentos tóxicos, comidas que cães e gatos podem comer e entre outras informações.

---
+++ dados.js
@@ -0,0 +1,46 @@
+let dados = [
+    {
+        titulo : "Vacinas para gatos",
+        descricao : "Quais vacinas são essenciais para gatos? De acordo com as diretrizes da WSAVA, os felinos precisam ser protegidos contra as seguites doenças: Panleucopenia felina, Calicivirose felina, Herpesvírus felino tipo 1 e Raiva. Comercialmente essas vacinas são conhecidas como V5 ou V3. Com quantos anos meu gato precisa iniciar a vacinação? A partir das 6 semanas de vida. Quantas doses meu gatinho precisa tomar? Os felinos precisam tomar múltiplas doses da vacina até completarem 16 semanas de vida, sendo que geralmente o intervalo entre essas doses é de 21 dias.",
+        tags: "felino v3 felv fiv raiva idade anos meses"
+    },
+
+    {
+        titulo : "Vacinas para cães",
+        descricao : "Quais vacinas são essenciais para cães? Os cachorrinhos devem tomar vacinas que ofereçam proteção contra as seguintes doenças: Cinomose, Hepatite infeciosa canina, Parvovirose e Raiva. Qual a idade em que o cão deve ser vacinado? A partir das 6 semanas de vida. Ele precisa tomar somente uma dose ou várias? O cachorrinhos precisam tomar múltiplas doses de vacina até completarem 16 semanas de vida, sendo que o intervalo entre cada dose geralmente é de 21 dias. Meu cachorro pode sair para rua com uma dose da vacina? Não, os cães só podem sair para passear após tomarem todas as vacinas necessárias, ou seja, após completarem as 16 semanas de vida e estarem com a carteira de vacinação em dia.",
+        tags: "cachorro cão cao vacina vacinação raiva parvo cinomose parvovirose idade" 
+    },
+
+    {
+        titulo : "Teste de Fiv e Felv",
+        descricao : "O que é Fiv e Felv? Fiv (Vírus da Imunodeficiência felina) e Felv (Vírus da Leucemia Felina), são doenças imunossupressoras, ou seja, doenças que afetam o sistema imunológico dos felinos deixando eles mais propenços a terem infecções e doenças, assim como o surgimento de tumores. Ambas doenças reduzem a expectativa de vida dos gatinhos. Como saber se meu gato tem essas doenças? Através de Kits de testes rápidos (semelhantes aos de COVID) é possível saber se o seu gatinho tem essas doenças. E se meu gato testar positivo o que eu faço? Nesse caso, ele deve passar por um veterinário para receber as devidas recomendções e acompanhamento médico",
+        tags: "Fiv felv aids felinos gatos" 
+    },
+
+    {
+        titulo : "Alimentos tóxicos para cães e gatos",
+        descricao : "Quais alimentos eu não posso dar para meus pets? Dentre os alimentos que são tóxicos para cães e gatos estão: Uvas, chocolate, café, alho, cebola, tomate verde.",
+        tags: "cachorro cão gato alimentos comida tóxico intoxicação" 
+    },
+
+    {
+        titulo : "Alimentos que cães podem comer",
+        descricao : "Quais alimentos meu cachorro pode comer? Frango cozido sem sal, carnes num geral desde que sejam cozidas e não temperadas. Legumes como cenoura, chuchu, batata doce, abobrinha. Frutas como maçã, banana, melância, manga... Meu cachorro pode passar a comer somente comida? Sim, entretanto ele precisa ser avaliado por um médico veterinário nutricionista, no qual irá criar uma dieta balanceada para o seu pet, evitando que ele sofra com falta de nutrientes e vitaminas.",
+        tags: "frutas cachorro legumes comida" 
+    },
+
+    {
+        titulo : "Alimentos que gatos podem comer",
+        descricao : "Quais alimentos meu gato pode comer? Frango cozido sem sal, carnes num geral desde que sejam cozidas e não temperadas. Legumes como cenoura, abóbora, batata doce. Frutas como maçã, morango, melância...",
+        tags: "frutas gatos legumes comida" 
+    },
+
+    {
+        titulo : "Tártaro em pets",
+        descricao : "Meu pet precisa escovar os dentes? Sim, os animais também precisam fazer sua higiene bucal, pois assim evita a formação de tártaro (placa bacteriana). E como faço para escovar os dentes dele? Da mesma forma que no ser humano, ou seja, utilizando escova de dente e pasta dental (própria para cães e gatos). E se eu não conseguir escovar os dentes dele o que devo fazer? Nesse caso, ele precisa ser acompanhado por um médico veterinário e realizar limpeza dentária periódicamente para rever o tártaro e impedir que ele adquira doenças secundárias. Quais doenças meu pet pode ter devido ao tártaro? Seu pet pode desenvolver inflamação e sangramento na gengiva, perda de dentes, abcessos dentários, e até mesmo problemas no coração e nos rins",
+        tags: "dentes bafo escovar tártaro doença" 
+    },
+   
+   
+];
+
